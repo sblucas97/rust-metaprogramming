ab79fb53fb45a6cfc4f28758be818232c83e792d
bkinglucas97 <bking.lucas97@gmail.com>
2025-07-24 22:42:50 -0300

    dynamically generating a c function and using it

---
+++ app/src/bin/dynamic_c.rs
@@ -0,0 +1,40 @@
+use libloading::{Library, Symbol};
+use std::{fs::write, process::Command};
+
+
+fn main() {
+    let new_c_function = r#"
+        #include<stdio.h>
+
+        int mult(int a, int b) { return a * b; } 
+
+        int main() {
+            return 0;
+        }
+    "#;
+
+    let file_path = "dynamic.c";
+
+    write(file_path, new_c_function).expect("Failed to create file");
+
+    // compile to shared object
+    let so_path = "./libdyn.so";
+    let output = Command::new("gcc")
+        .args(["-shared", "-fPIC", file_path, "-o", so_path])
+        .output()
+        .expect("Failed to compile shared object");
+
+    if !output.status.success() {
+        eprintln!("GCC Error: {}", String::from_utf8_lossy(&output.stderr));
+        return;
+    }
+
+    unsafe {
+        let lib = Library::new(so_path).expect("Failed to load lib");
+        let mult: Symbol<unsafe extern "C" fn(i32, i32) -> i32> =
+            lib.get(b"mult").expect("Failed to find symbol");
+        
+        let result = mult(12, 12);
+        println!("{}", result);
+    }
+}
