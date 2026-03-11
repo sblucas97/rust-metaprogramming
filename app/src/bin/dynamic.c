aa639441cb4b5b2f0f259fe8bf42b39022476d61
Lucas Barbosa <bking.lucas97@gmail.com>
2026-03-11 00:05:39 -0300

    chore: cleanup unnused files

--- app/src/bin/dynamic.c
+++
@@ -1,9 +0,0 @@
-
-        #include<stdio.h>
-
-        int mult(int a, int b) { return a * b; } 
-
-        int main() {
-            return 0;
-        }
-    
--- app/src/dynamic.c
+++
@@ -1,3 +0,0 @@
-
-        int mult(int a, int b) { return a * b; } 
-    
